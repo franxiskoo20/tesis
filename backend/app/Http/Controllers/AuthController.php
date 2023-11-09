eaada79be7cd355233270206ad1fd77f0cfc0e70
franxi <franciscoz.x123@gmail.com>
2023-11-09 15:55:50 -0300

    implementar patron de repositorio

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller

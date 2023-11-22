c042965ac5d4f0432df592b9092899eb5f3b7b16
franxiskoo20 <francisco.gonzalez1701@alumnos.ubiobio.cl>
2023-11-22 20:10:08 -0300

    frontend: agregar iconos a la barra

--- frontend/src/components/layout/AuthenticatedLayout.jsx
+++ frontend/src/components/layout/AuthenticatedLayout.jsx
@@ -1,8 +1,8 @@
 import NavigationBar from "../common/NavigationBar";
 import { Button, Container, Box } from "@mui/material";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleIcon from '@mui/icons-material/People';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import useAuth from "../../features/auth/useAuth";
 import { Link } from "react-router-dom";
@@ -18,20 +18,20 @@ export default function AuthenticatedLayout({ children }) {
       { name: "Usuarios", path: "/app/users", icon: <PeopleIcon /> },
     ],
     [ROLES.JEFE_COMERCIAL]: [
-      { name: "Dashboard", path: "/app/dashboard" },
-      { name: "Products", path: "/app/products" },
+      { name: "Dashboard", path: "/app/dashboard", icon: <DashboardIcon /> },
+      { name: "Products", path: "/app/products", icon: <ShoppingCartIcon /> },
     ],
     [ROLES.CUSTOMER_SERVICE]: [
-      { name: "Dashboard", path: "/app/dashboard" },
-      { name: "Products", path: "/app/products" },
+      { name: "Dashboard", path: "/app/dashboard", icon: <DashboardIcon /> },
+      { name: "Products", path: "/app/products", icon: <ShoppingCartIcon /> },
     ],
     [ROLES.ROMANA]: [
-      { name: "Dashboard", path: "/app/dashboard" },
-      { name: "Products", path: "/app/products" },
+      { name: "Dashboard", path: "/app/dashboard", icon: <DashboardIcon /> },
+      { name: "Products", path: "/app/products", icon: <ShoppingCartIcon /> },
     ],
     [ROLES.PORTERO]: [
-      { name: "Dashboard", path: "/app/dashboard" },
-      { name: "Products", path: "/app/products" },
+      { name: "Dashboard", path: "/app/dashboard", icon: <DashboardIcon /> },
+      { name: "Products", path: "/app/products", icon: <ShoppingCartIcon /> },
     ],
   };
 
@@ -41,7 +41,13 @@ export default function AuthenticatedLayout({ children }) {
 
   const roleBasedNavigation = (role) => {
     return navigationByRole[role]?.map((item) => (
-      <Button key={item.name} color="inherit" component={Link} to={item.path} startIcon={item.icon}>
+      <Button
+        key={item.name}
+        color="inherit"
+        component={Link}
+        to={item.path}
+        startIcon={item.icon}
+      >
         {item.name}
       </Button>
     ));

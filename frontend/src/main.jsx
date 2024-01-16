3db2e6181773c6b5ee0eea8f83c64cdddf8cef23
franxiskoo20 <francisco.gonzalez1701@alumnos.ubiobio.cl>
2024-01-15 21:30:00 -0300

    final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/style/fontStyle.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools />
+    {/* <ReactQueryDevtools /> */}
     <App />
   </QueryClientProvider>
   // </React.StrictMode>,
